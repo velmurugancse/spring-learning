d75671f7d910b5f595984f3d04f8745b5f7d86c5
Velmurugan A <velmurugan@mail.asahitechnologies.com>
2019-02-14 13:19:13 +0530

    Spring java annotation demo for IOC

---
+++ spring_annotation_demo/src/springdemo/TennisCoach.java
@@ -0,0 +1,14 @@
+package springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+
+	
+	@Override
+	public String getDailyWorkout() {
+		return "Practice your backhand volley";
+	}
+
+}
