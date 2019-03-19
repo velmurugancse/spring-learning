448d00ee64c7825c99bf95b96e79dd1cfdb9d7ab
hrajendra <haritha@asahitechnologies.com>
2019-03-19 19:37:22 +0530

    Custom login page for security

---
+++ spring-security-custom_login/src/main/java/com/luv2code/springsecurity/demo/controller/DemoController.java
@@ -0,0 +1,24 @@
+package com.luv2code.springsecurity.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+	@GetMapping("/")
+	public String showHome() {
+		
+		return "home";
+	}
+	
+	@GetMapping("/showMyLoginPage")
+	public String showMyLoginPage() {
+		
+		return "plain-login";
+		
+	}
+	
+	
+	
+}
